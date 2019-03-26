125604efc63c9ccb7f65fd2ed4bdae6426453cd4
Juliettech <juliettech.1393@gmail.com>
2019-03-26 11:48:33 -0500

    completed basic search with query params and form_tag

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -2,13 +2,27 @@ class SentencesController < ApplicationController
   def index
   	# @sentences = Sentence.all
   	# raise
+    @query = params[:query]
   	if params[:query].present?
   		sql_query = " \
   		sentences.name @@ :query \
-
+      OR sentences.category @@ :query \
+      OR sentences.institution @@ :query \
+      OR sentences.entry_point @@ :query \
+      OR bodies.content @@ :query \
+      OR bodies.category @@ :query
   		"
-  		@sentences = Sentence.where(sql_query, query: "%#{params[:query]}%")
-  		# @sentences = Sentence.joins(:bodies).where(sql_query, query: "%#{params[:query]}%")
+
+      # movies.title ILIKE :query \
+      #       OR movies.syllabus ILIKE :query \
+      #       OR directors.first_name ILIKE :query \
+      #       OR directors.last_name ILIKE :query \
+  		# @sentences = Sentence.where(sql_query, query: "%#{params[:query]}%")
+      # @sentences = @sentences.uniq
+      # @sentences.each do |sentence|
+      #   if sentence.uniq(@sentences)
+      # end
+  		@sentences = Sentence.joins(:bodies).where(sql_query, query: "%#{params[:query]}%")
   		# raise
   	else
   		@sentences = Sentence.all
