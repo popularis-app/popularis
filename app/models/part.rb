59aff953b9b941b01c2eefa5c8f37c62d765f02c
Juliettech <juliettech.1393@gmail.com>
2019-03-27 17:10:31 -0500

    added date method and capitalization to sentence model + added first_name method to parts model

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,3 +1,18 @@
 class Part < ApplicationRecord
   belongs_to :sentence
+
+
+  def capitalize_everything(attribute)
+    words = attribute.split
+    words.map{ |word| word.capitalize }.join(" ")
+  end
+
+
+  def first_name
+    if name.blank?
+      ""
+    else
+      name.split.first.capitalize
+    end
+  end
 end
