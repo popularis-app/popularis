c0642fa2fe61422dc824c9f51762967f1b76d26a
Juliettech <juliettech.1393@gmail.com>
2019-04-01 16:55:05 -0500

    made sure all edge cases in the search were taken care of. Specifically, made sure the sentences#index never got an error message if the query input wasn't found from body.content but rather all other possibilities found in the sentences_controller. Also made sure the menu didn't move and always stayed on right.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,7 @@
 import "bootstrap";
+
+// const homepage = document.querySelector('.pages.home')
+// if(homepage){
+//   const menuItemsHome = document.querySelector('html body.pages.home div.navbar.navbar-expand-sm.navbar-light');
+//   console.log(menuItemsHome)
+// }
