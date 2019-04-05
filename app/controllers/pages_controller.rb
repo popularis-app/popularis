196318c10b3dac8fe79a9df3dceed67ebe1b6fa9
Juliettech <juliettech.1393@gmail.com>
2019-04-05 13:04:16 -0500

    pages#about_us design done for desktop. Still missing responsiveness in .popularis-info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,12 @@ require 'open-uri'
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
+
   def home
   end
 
   def about_us
-  @members = parsed_members
+    @members = parsed_members
   end
 
   def terms
