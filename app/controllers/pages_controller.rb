623f0b993c34b74a98a21cf6b8f7e03b7cfc0f79
Juliettech <juliettech.1393@gmail.com>
2019-04-08 15:40:12 -0500

    adapted pages#about_us method to fit new api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,13 +7,14 @@ class PagesController < ApplicationController
   end
 
   def about_us
-    @members = parsed_members
+    @members = parsed_members['Equipo']
   end
 
   def terms
   end
 
   private
+
   def parsed_members
     url = 'https://juliet-tech.github.io/popularis-api/team/team_members.json'
     serialized_team = open(url).read
