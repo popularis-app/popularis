704abc1eec7722f36947124794f83bb949f18a7e
Juliettech <juliettech.1393@gmail.com>
2019-04-09 15:04:01 -0500

    added amount of times query word appears inside of that sentence

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -14,22 +14,10 @@ class SentencesController < ApplicationController
   		"
 
       # Try putting % % around :query -> https://sqlbolt.com/lesson/select_queries_with_constraints_pt_2
-  		@sentences = Sentence.joins(:bodies).where(sql_query, query: "%#{params[:query]}%")
-      @query_qty = @sentences.each_with_object(Hash.new(0)) { |sentence, counts| counts[sentence] += 1 }
+  		@raw_sentences = Sentence.joins(:bodies).where(sql_query, query: "%#{params[:query]}%")
+      @query_qty = @raw_sentences.each_with_object(Hash.new(0)) { |sentence, counts| counts[sentence] += 1 }
       @sorted_sentences = @query_qty.sort_by { |sentence, qty| qty }.reverse
       @sentences = @sorted_sentences.map { |e| e[0] }
-
-
-      # @sentences = @sentences.sort_by { |sentence| @sentences.grep(sentence).size }
-
-      # @sentences.each do |sentence|
-      #  sentence.bodies.map do |body|
-      #     body.content.count(@query)
-      #   end
-      # end
-
-      # Antes de solo display las .uniq -> orderlas en orden dependiendo de la que mas salga repetida.... weighted display on index
-      # @sentences = @sentences.uniq
   	else
   		@sentences = Sentence.all
   	end
