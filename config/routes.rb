00cfd89e8d48f0112bcb632391475359264a0c01
Juliettech <juliettech.1393@gmail.com>
2019-04-17 09:35:42 -0500

    added encrypted path to website's information

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   get 'about_us', to: 'pages#about_us'
   get 'terms', to: 'pages#terms'
   get 'updating_db', to: 'sentences#updating_db'
+  get 'jYai98Pp7', to: 'feedbacks#index', as: :feedbacks
 
   resources :sentences, only: [:index, :show]
-  resources :feedbacks, only: [:index, :new, :create]
+  resources :feedbacks, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
