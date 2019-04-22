10eb300b027fd3c433341f2135aedf094def9673
Juliettech <juliettech.1393@gmail.com>
2019-04-22 13:47:09 -0500

    changed path about_is to sobre_nosotros for spanish understanding

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
-  get 'about_us', to: 'pages#about_us'
+  get 'sobre_nosotros', to: 'pages#about_us', as: :about_us
   get 'terms', to: 'pages#terms'
   get 'updating_db', to: 'sentences#updating_db'
   get 'jYai98Pp7', to: 'feedbacks#index'
 
   resources :sentences, only: [:index, :show]
-  resources :feedbacks, only: [:index, :new, :create]
+  resources :feedbacks, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
